1399d863288a226b6ca30aa3c61c68de52e3869f
Cortlando <cortlandervins@gmail.com>
2025-06-11 16:34:48 -0500

    Finished chapter 3 and completed the project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words, num_of_letter, seperate_words
+from stats import get_num_words, num_of_letter, seperate_words, sort_letter_by_count
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,13 +7,26 @@ def get_book_text(filepath):
     return file_contents
     
 def main():
-    filepath = 'books/frankenstein.txt'
-    contents = get_book_text(filepath)
-    # print(contents)
-    num_words = get_num_words(contents)
-    print(f'{num_words} words found in the document')
-    num_of_letters = num_of_letter(seperate_words(contents))
-    print(num_of_letters)
     
+    if len(sys.argv) != 2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
+    
+    filepath = sys.argv[1]
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {filepath}...')
+    contents = get_book_text(filepath)
+   
+    print('----------- Word Count ----------')
+    num_words = get_num_words(contents)
+    print(f'Found {num_words} total words')
+    
+    print('--------- Character Count -------')
+    num_of_letters = num_of_letter(seperate_words(contents))
+    # print(num_of_letters)
+    sorted_list = sort_letter_by_count(num_of_letters)
+    for dict in sorted_list:
+        print(f'{dict['char']}: {dict['num']}')
+    print('============= END ===============')
     
 main()
--- stats.py
+++ stats.py
@@ -17,5 +17,25 @@ def num_of_letter(book_text):
                 letter_count[letter] = 1
     
     return letter_count
-            
+ 
+ 
+def sort_on(dict):
+    return dict["num"]
+           
+def sort_letter_by_count(letter_count):
+    # print(letter_count)
+    letter_list= []
+    
+    for letter in letter_count:
+        if letter.isalpha():
+            temp = {
+                "char" : letter,
+                "num" : letter_count[letter]
+            }
+            letter_list.append(temp)
+    
+    letter_list.sort(reverse=True, key=sort_on)
+    return letter_list
+    
+        
     
